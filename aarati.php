b88d73bd7af74034183b212be83dca1112be5706
Rutujacoder <70790239+Rutujacoder@users.noreply.github.com>
2020-10-03 19:46:27 +0530

    Add files via upload

---
+++ aarati.php
@@ -0,0 +1,706 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ganesha</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<!-- CSS only -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.1/css/bootstrap.min.css" integrity="sha384-VCmXjywReHh4PwowAiWNagnWcLhlEJLA5buUprzK8rxFgeH0kww/aWY76TfkUoSX" crossorigin="anonymous">
+
+<!-- JS, Popper.js, and jQuery -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.1/js/bootstrap.min.js" integrity="sha384-XEerZL0cuoUbHE4nZReLT7nx9gQrQreJekYhJD9WNWhH8nEW+0c5qq7aIo2Wl30J" crossorigin="anonymous"></script>
+ <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+ <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+ <style>
+@import url('https://fonts.googleapis.com/css2?family=Teko:wght@400;700&display=swap');
+    *{
+    	padding: 0;
+    	margin: 0;
+    	
+    }
+	body{
+		background-color: white;
+		
+		margin: 0;
+		padding:0;
+		font-family: sans-serif;
+	}
+	
+	
+
+   html{
+		scroll-behavior: smooth;
+	}
+	
+	.gototop{
+		position: fixed;
+		width: 50px;
+		height: 50px;
+		background:black;
+		bottom: 40px;
+		right: 50px;
+		text-decoration: none;
+		text-align: center;
+		line-height: 50px;
+		color: white;
+		font-size: 32px;
+		z-index: 1;
+	}
+	#rj{
+		height: 100px;
+		float: left;
+		margin: 14px 60px;
+	}
+	
+	
+	.row{
+		border-bottom: 10px solid rgba(255 ,255,255,0.3);
+		line-height: 20px;
+
+		background-color:white;
+	}
+	
+	pre{
+		font-family: 'Teko', sans-serif;
+		text-align: justify;
+		color: black;
+		word-spacing: 10px;
+		line-height: 40px;
+		font-size: 24px;
+	}
+	video{
+		width:400px;
+		height: 400px;
+		
+		
+	}
+	
+	.dec{
+		width: 100%;
+    	height: 100%;
+    	color: #303030;
+    background: white;
+    	position: relative;
+    	top: px;
+      box-shadow: 4px 4px 4px 4px rgba(200,190,100,0.9);
+	}
+	
+
+h5{
+	color: #DE7C1F;
+}
+.nav-item,.nav-link{
+		font-size: 20px;
+		padding-left: 10px;
+		color: blue;
+	}
+	.navbar-toggler-icon{
+	float: right;
+
+	color: blue;
+
+
+
+}
+
+</style>
+</head>
+<body>
+	<div class="container-fluid">
+ 
+		<nav class="navbar navbar-expand-sm" style="background-color: black;">
+			<img src="ganpati.jpg" class="img-fluid" style="width: 100px;height: 70px; float: left;" class="logo">
+            <div class="container">
+            	<p>&nbsp;&nbsp;</p>
+	   
+
+	  <div style="position: relative;top:-70px;">
+	   <button class="navbar-toggler" data-toggle="collapse" data-target="#gan">
+	   	<span class="navbar-toggler-icon"><i class="fa fa-bars" aria-hidden="true"></i></span>
+	   </div>
+	   	
+
+	   </button>
+	   		<div class="collapse navbar-collapse" id="gan">
+	   	<ul class="navbar-nav nav-tabs text-center ml-auto">
+	   		<li class="nav-item"><a href="index.php" class="nav-link">HOME</a></li>
+	   		<li class="nav-item"><a href="story.php" class="nav-link">STORY</a></li>
+	   		<li class="nav-item"><a href="aarati.php" class="nav-link active">AARTI</a></li>
+	   		<li class="nav-item"><a href="product.php" class="nav-link">PRODUCTS</a></li>
+	   		<li class="nav-item"><a href="form.php" class="nav-link">ORDER</a></li>
+</ul>
+</div>	
+</nav>   	
+
+<a href="#" class="gototop">
+<i class="fas fa-arrow-up"></i></a>
+<div class="dec">
+	<div class="container">
+		<br><br>
+<div class="row">
+	<div class="col-md-4">
+		<pre>
+<h5 style="font-size: 45px;">सुख करता दुखहर्ता</h5>	
+सुख करता दुखहर्ता, वार्ता विघ्नाची
+नूर्वी पूर्वी प्रेम कृपा जयाची
+सर्वांगी सुन्दर उटी शेंदु राची
+कंठी झलके माल मुकताफळांची
+
+जय देव जय देव, जय मंगल मूर्ति
+दर्शनमात्रे मनःकमाना पूर्ति
+जय देव जय देव
+
+रत्नखचित फरा तुझ गौरीकुमरा
+चंदनाची उटी कुमकुम केशरा
+हीरे जडित मुकुट शोभतो बरा
+रुन्झुनती नूपुरे चरनी घागरिया
+
+		</pre>
+</div>
+
+	<div class="col-md-4">
+		<pre> 
+		
+जय देव जय देव, जय मंगल मूर्ति
+दर्शनमात्रे मनःकमाना पूर्ति
+जय देव जय देव
+
+लम्बोदर पीताम्बर फनिवर वंदना
+सरल सोंड वक्रतुंडा त्रिनयना
+दास रामाचा वाट पाहे सदना
+संकटी पावावे निर्वाणी रक्षावे सुरवर वंदना
+
+जय देव जय देव, जय मंगल मूर्ति
+दर्शनमात्रे मनःकमाना पूर्ति
+जय देव जय देव
+		</pre>
+</div>
+<div class="col-md-4">
+	<video controls>
+		<source src="sukhakrta.mp4" class="img-fluid">
+		</video>
+</div>
+</div>
+<hr>
+<div class="row">
+
+	<div class="col-md-4">
+		<pre>
+<h5 style="font-size: 50px;">देवा श्री गणेशा</h5>
+देवा श्री गणेशा, देवा श्री गणेशा
+देवा श्री गणेशा, देवा श्री गणेशा
+देवा श्री गणेशा, देवा श्री गणेशा
+देवा श्री गणेशा, देवा श्री गणेशा
+देवा श्री गणेशा, देवा श्री गणेशा
+ज्वाला सी जलती है आँखो में
+जिसके भी दिल मे तेरा नाम है
+परवाह ही क्या उसका आरंभ कैसा है
+और कैसा परिणाम है
+धरती अंबर सितारे
+उसकी नज़रे उतारे
+डर भी उससे डरा रे
+जिसकी रखवालिया रे
+करता साया तेरा हे
+
+		</pre>
+</div>
+	
+	<div class="col-md-4">
+		<pre>
+			
+देवा श्री गणेशा देवा श्री गणेशा
+देवा श्री गणेशा देवा श्री गणेशा
+देवा श्री गणेशा देवा श्री गणेशा
+देवा श्री गणेशा देवा श्री गणेशा
+हो तेरी भक्ति तो वरदान है
+जो कमाए वो धनवान है
+बिन किनारे की कश्ती है वो
+देवा तुझसे जो अन्जान है
+यूँ तो मूषक सवारी तेरी
+सब पे है पहेरेदारी तेरी
+पाप की आँधिया लाख हो
+कभी ज्योती ना हारी तेरी
+अपनी तकदीर का वो खुद सिकंदर हुआ रे
+भूल के ये जहां रे
+जिस किसी ने यहाँ रे साथ पाया तेरा
+हे देवा श्री गणेशा…
+		</pre>
+</div>
+<div class="col-md-4">
+	<br><br>
+	<video controls>
+		<source src="ajubhau.mp4" class="img-fluid">
+	</video>
+</div>
+</div>
+<hr>
+<div class="row">
+	<div class="col-md-4">
+		<pre>
+			<pre> <h5 style="font-size: 50px;">जय जय जी गणराज</h5>	
+
+शेंदुर लाल चढायो अच्छा गजमुख को
+दोन्दिल लाल बिराजे सूत गौरिहर को
+हाथ लिए गुड लड्डू साई सुरवर को
+महिमा कहे ना जाय लागत हूँ पद को
+
+जय जय जय जय जय
+जय जय जी गणराज विद्यासुखदाता
+धन्य तुम्हारो दर्शन मेरा मत रमता
+जय देव जय देव
+
+अष्ट सिधि दासी संकट को बैरी
+विघन विनाशन मंगल मूरत अधिकारी
+कोटि सूरज प्रकाश ऐसे छबी तेरी
+गंडस्थल मद्मस्तक झूल शशि बहरी
+
+		</pre>
+</div>
+
+	<div class="col-md-4">
+		<br><br>
+		<pre>
+जय जय जय जय जय
+जय जय जी गणराज विद्यासुखदाता
+धन्य तुम्हारो दर्शन मेरा मत रमता
+जय देव जय देव
+
+भावभगत से कोई शरणागत आवे
+संतति संपत्ति सबही भरपूर पावे
+ऐसे तुम महाराज मोको अति भावे
+गोसावीनंदन निशिदिन गुण गावे
+
+जय जय जी गणराज विद्यासुखदाता
+धन्य तुम्हारो दर्शन मेरा मत रमता
+जय देव जय देव
+
+		</pre>
+</div>
+<div class="col-md-4">
+	<video controls>
+		<source src="ja1.mp4" class="img-fluid">
+		</video>
+
+</div>
+</div>
+<hr>
+<div class="row">
+	<div class="col-md-4">
+		<pre>
+<pre> <h5 style="font-size: 50px;">एकदंताय वक्रतुण्डाय</h5>
+
+गणनायकाय गणदेवताय गणाध्यक्षाय धीमहि 
+गुणशरीराय गुणमण्डिताय गुणेशानाय धीमहि 
+गुणातीताय गुणाधीशाय गुणप्रविष्टाय धीमहि 
+एकदंताय वक्रतुण्डाय गौरीतनयाय धीमहि 
+गजेशानाय भालचन्द्राय श्रीगणेशाय धीमहि 
+
+गानचतुराय गानप्राणाय गानान्तरात्मने 
+गानोत्सुकाय गानमत्ताय गानोत्सुकमनसे 
+गुरुपूजिताय गुरुदेवताय गुरुकुलस्थायिने 
+गुरुविक्रमाय गुह्यप्रवराय गुरवे गुणगुरवे 
+गुरुदैत्यगलच्छेत्रे गुरुधर्मसदाराध्याय 
+गुरुपुत्रपरित्रात्रे गुरुपाखण्डखण्डकाय 
+गीतसाराय गीततत्त्वाय गीतगोत्राय धीमहि 
+गूढगुल्फाय गन्धमत्ताय गोजयप्रदाय धीमहि 
+गुणातीताय गुणाधीशाय गुणप्रविष्टाय धीमहि 
+एकदंताय वक्रतुण्डाय गौरीतनयाय धीमहि 
+गजेशानाय भालचन्द्राय श्रीगणेशाय धीमहि 
+
+
+		</pre>
+</div>
+
+	<div class="col-md-4">
+			<pre>
+ग्रन्थगीताय ग्रन्थगेयाय ग्रन्थान्तरात्मने
+गीतलीनाय गीताश्रयाय गीतवाद्यपटवे
+गेयचरिताय गायकवराय गन्धर्वप्रियकृते
+गायकाधीनविग्रहाय गङ्गाजलप्रणयवते
+गौरीस्तनन्धयाय गौरीहृदयनन्दनाय
+गौरभानुसुताय गौरीगणेश्वराय
+गौरीप्रणयाय गौरीप्रवणाय गौरभावाय धीमहि
+गोसहस्राय गोवर्धनाय गोपगोपाय धीमहि
+गुणातीताय गुणाधीशाय गुणप्रविष्टाय धीमहि 
+एकदंताय वक्रतुण्डाय गौरीतनयाय धीमहि 
+गजेशानाय भालचन्द्राय श्रीगणेशाय धीमहि 
+		</pre>
+</div>
+<div class="col-md-4">
+	<video controls>
+		<source src="ek.mp4" class="img-fluid">
+	</video>
+	
+</div>
+</div>
+<hr>
+<div class="row">
+
+	<div class="col-md-4">
+		<pre>
+<h5 style="font-size: 50px;">रांजण गावाला, गावाला</h5>
+
+1. रांजण गावाला, गावाला
+महा गणपती नांदला
+4 5 6 (गण गण, गण गणपती नांदला)
+2. रांजण गावाला, गावाला
+महा गणपती नांदला
+1 4 5(गण गण, गण गणपती नांदला)
+3 चला पहाटे, पहाटे
+देव केव्हाचा जागला
+♫♪♫♪♫♪♫
+(1 2 गणपती नांदला)
+♫♪♫♪♫♪♫
+4. त्रिपुरासुर ऐसा कोपे
+शिवशंकराला टोपे
+5. त्रिपुरासुर ऐसा कोपे
+शिवशंकराला टोपे
+6. त्रिपुरासुर ऐसा कोपेssss
+शिवशंकराला टोपे
+1. पुत्र गणपती गणपती
+रणी जयाला धावला..(2. धावला..धावला)
+♫♪♫♪♫♪♫
+
+
+		</pre>
+</div>
+	
+	<div class="col-md-4">
+		<pre>
+			3. रांजण गावाला गावाला
+महा गणपती नांदला
+1 4 5(गण गण, गण गणपती नांदला)
+2. रांजण गावाला, गावाला
+महा गणपती नांदला
+
+1 3 6 (गण गण, गण गणपती नांदला)
+5 चला पहाटे, पहाटे
+देव केव्हाचा जागला
+2. त्रिपुराचा नाश केला
+गण येथेचि राहिला
+3. त्रिपुराचा नाश केला
+गण येथेचि राहिला
+4. त्रिपुराचा नाश केला
+गण येथेचि राहिला
+6. दहा शुंडांचा शुंडांचा
+वीसा भुजांचा शोभला.. (3. शोभला.. शोभला)
+♫♪♫♪♫♪♫
+1 2 रांजण गावाला गावाला
+महा गणपती नांदला
+5 6 (गण गण, गण गणपती नांदला)
+3 4 रांजण गावाला, गावाला
+महा गणपती नांदला
+5 6 (गण गण, गण गणपती नांदला)
+all चला पहाटे, पहाटे
+देव केव्हाचा जागला.
+
+		</pre>
+</div>
+<div class="col-md-4">
+	<video controls>
+		<source src="raga.mp4" class="img-fluid">
+		</video>
+</div>
+</div>
+<hr>
+<div class="row">
+
+	<div class="col-md-4">
+		<pre>
+<h5 style="font-size: 50px;">मोरया मोरया मोरया रे</h5>
+
+मेरे सारे पलछिन सारे दिन
+तरसेंगे सुन ले तेरे बिन
+
+तुझको फिर से जलवा दिखाना ही होगा
+अगले बरस आना है, आना ही होगा
+तुझको फिर से जलवा दिखाना ही होगा
+अगले बरस आना है, आना ही होगा
+
+देखेंगी तेरी राहे,
+प्यासी प्यासी निगाहे
+तो मान ले, तू मान भी ले
+तू कहना मेरा
+
+लौट के तुझे आना है,
+सुन ले कहता दीवाना है
+जब तेरा दर्शन पाएंगे,
+चैन तब हमको पाना है
+
+मोरया मोरया, मोरया रे
+बाप्पा मोरया, मोरया मोरया रे
+मोरया मोरया, मोरया रे
+बाप्पा मोरया, मोरया मोरया रे
+
+
+
+		</pre>
+</div>
+	
+	<div class="col-md-4">
+		<pre>
+हो खुशियों के दिन हो, के गम का जमाना
+दिल बस लेता है नाम तेरा
+तेरे ही कारण है जीवन सुहाना
+तुही तो मन में तन मन में बसा
+हर घडी ध्यान रहे तेरा
+
+मै हु तेरा चाहने वाला
+जपता हु तेरी माला
+तो मान ले, तू मान भी ले,
+तू कहना मेरा
+
+लौट के तुझे आना है,
+सुन ले कहता दीवाना है
+जब तेरा दर्शन पाएंगे,
+चैन तब हमको पाना है
+
+मोरया मोरया मोरया रे
+बाप्पा मोरया मोरया मोरया रे
+मोरया मोरया मोरया रे
+बाप्पा मोरया मोरया मोरया रे
+
+Tujhko phir se jalwa dikhana hi hoga
+मोरया मोरया बाप्पा मोरया रे
+तुझको फिर से जलवा दिखाना ही होगा
+अगले बरस आना है आना ही होगा
+		</pre>
+</div>
+<div class="col-md-4">
+	<video controls>
+		<source src="mo.mp4" class="img-fluid">
+		</video>
+</div>
+</div>
+<hr>
+<div class="row">
+
+	<div class="col-md-4">
+		<pre>
+
+<h5 style="font-size: 50px;">जय गणेश जय गणेश</h5>
+
+जय गणेश जय गणेश
+जय गणेश देवा
+जय गणेश जय गणेश
+जय गणेश देवा
+माता ज्याकी पार्वती पिता महादेवा
+माता ज्याकी पार्वती पिता महादेवा
+जय गणेश जय गणेश
+जय गणेश देवा
+जय गणेश जय गणेश
+जय गणेश देवा
+माता ज्याकी पार्वती पिता महादेवा
+माता ज्याकी पार्वती पिता महादेवा
+एकदंत दयावंत चार भुजा धारी
+एकदंत दयावंत चार भुजा धारी
+माथेपर तिलक सोहे मुसे की सवारी
+माथेपर तिलक सोहे मुसे की सवारी
+एकदंत दयावंत चार भुजा धारी
+एकदंत दयावंत चार भुजा धारी
+माथेपर तिलक सोहे मुसे की सवारी
+माथेपर तिलक सोहे मुसे की सवारी
+
+
+
+		</pre>
+</div>
+	
+	<div class="col-md-4">
+		<pre>
+पान चढ़े फूल चढ़े और चढ़े मेवा
+पान चढ़े फूल चढ़े और चढ़े मेवा
+लढूअन का भोग लगे संत करे सेवा
+लढूअन का भोग लगे संत करे सेवा
+जय गणेश जय गणेश
+जय गणेश देवा
+जय गणेश जय गणेश
+जय गणेश देवा
+माता ज्याकी पार्वती पिता महादेवा
+माता ज्याकी पार्वती पिता महादेवा
+अंधे को आँख देत कोढिन को काया
+अंधे को आँख देत कोढिन को काया.
+जय गणेश जय गणेश
+जय गणेश देवा
+जय गणेश जय गणेश
+जय गणेश देवा
+माता ज्याकी पार्वती पिता महादेवा
+माता ज्याकी पार्वती पिता महादेवा
+
+		</pre>
+</div>
+<div class="col-md-4">
+	<video controls>
+		<source src="jai.mp4" class="img-fluid">
+		</video>
+</div>
+</div>
+<hr>
+<div class="row">
+
+	<div class="col-md-4">
+		<pre>
+
+<h5 style="font-size: 50px;">ओंकार स्वरुपा</h5>
+
+ओंकार स्वरुपा, ओंकार
+स्वरुपा, ओंकार स्वरुपा
+तुज नमो, तुज नमो, तुज
+नमो
+
+ओंकार स्वरुपा, सद्गुरु
+समर्था
+ओंकार स्वरुपा, सद्गुरु
+समर्था
+अनाथाच्या नाथा, तुज
+नमो, तुज नमो
+तुज नमो, तुज नमो, तुज
+नमो
+
+ओंकार स्वरुपा, सद्गुरु
+समर्था
+ओंकार स्वरुपा, सद्गुरु
+समर्था
+अनाथाच्या नाथा, तुज
+नमो, तुज नमो
+तुज नमो, तुज नमो, तुज
+नमो
+
+नमो मायबापा,
+गुरुकृपाघना
+तोडी या बंधना मायामोहा
+मोहोजाळ माझे कोण
+नीरशील
+तुजविण दयाळा
+सद्गुरुराया
+तुज नमो, तुज नमो, तुज
+नमो
+तुज नमो, तुज नमो, तुज
+नमो
+तुज नमो, तुज नमो, तुज
+नमोओंकार स्वरुपा, सद्गुरु
+समर्था
+ओंकार स्वरुपा, सद्गुरु
+समर्था
+अनाथाच्या नाथा, तुज
+नमो, तुज नमो
+तुज नमो, तुज नमो, तुज
+नमो
+
+सद्गुरुराया माझा
+आनंदसागर
+त्रैलोक्या आधार
+गुरुराव
+त्रैलोक्या आधार
+गुरुराव
+त्रैलोक्या आधार
+गुरुराव
+गुरुराव स्वामी असे
+स्वयंप्रकाश
+
+
+
+		</pre>
+</div>
+	
+	<div class="col-md-4">
+		<pre>
+गुरुराव स्वामी असे
+स्वयंप्रकाश
+ज्या पुढे उदास चंद्र,
+रवी
+ज्या पुढे उदास चंद्र,
+रवी
+ज्या पुढे उदास चंद्र,
+रवी
+रवी, शशी, अग्नि, नेणती
+ज्या रुपा
+रवी, शशी, अग्नि, नेणती
+ज्या रुपा
+स्वप्रकाशरुपा नेणे
+वेद
+स्वप्रकाशरुपा नेणे
+वेद
+स्वप्रकाशरुपा नेणे
+वेद
+तुज नमो, तुज नमो
+स्वप्रकाशरुपा तुज नमो
+तुज नमो, तुज नमो, तुज
+नमो
+
+एका जनार्दनी
+गुरुपरब्रम्ह
+एका जनार्दनी
+गुरुपरब्रम्ह
+तयाचे पै नाम सदा मुखी
+तयाचे पै नाम सदा मुखी
+तयाचे पै नाम सदा मुखी
+तुज नमो, तुज नमो
+तयाचे पै नाम सदा मुखी
+तुज नमो, तुज नमो, तुज
+नमो
+
+ओंकार स्वरुपा, सद्गुरु
+समर्था
+ओंकार स्वरुपा, सद्गुरु
+समर्था
+अनाथाच्या नाथा, तुज
+नमो, तुज नमो
+तुज नमो, तुज नमो, तुज
+नमो
+तुज नमो, तुज नमो, तुज
+नमो
+तुज नमो, तुज नमो, तुज
+नमो
+
+
+		</pre>
+</div>
+<div class="col-md-4">
+	<video controls>
+		<source src="om.mp4" class="img-fluid">
+		</video>
+</div>
+</div>
+</div>
+</div>
+<footer class="text-success-700 body-font">
+  <div class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
+    <a class="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+      
+      <span class="ml-3 text-xl">Lord Ganesha</span>
+    </a>
+    
+    <span class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+      <a class="text-gray-500">
+      	<a href="index1.html">
+        <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+          <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+        </svg>
+    </a>
+      </a>
+      <a class="ml-3 text-gray-500">
+        <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+          <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+        </svg>
+      </a>
+      <a class="ml-3 text-success-500">
+        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+          <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+          <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+        </svg>
+      </a>
+      <a class="ml-3 text-gray-500">
+        <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="0" class="w-5 h-5" viewBox="0 0 24 24">
+          <a href="https://www.linkedin.com/in/rutuja-deshmukh-6306481b6"><path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"></path></a>
+          <circle cx="4" cy="4" r="2" stroke="none"></circle>
+        </svg>
+      </a>
+    </span>
+  </div>
+</footer>
+</div>
+</body>
+</html>
